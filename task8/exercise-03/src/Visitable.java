49ddad0acba7825d53f695de19dc6360ce6d2ff6
BYT <s16500@pjwstjk.edu.pl>
2021-02-05 01:55:00 +0100

    BYTafter9

--- task8/exercise-03/src/Visitable.java
+++
@@ -1,4 +0,0 @@
-public interface Visitable 
-{
-	int accept(Visitor visitor);
-}
--- task8/exercise-03/src/Visitor.java
+++
@@ -1,8 +0,0 @@
-public interface Visitor 
-{
-	int visit(AddExpression expr);
-	int visit(SubtractExpression expr);
-	int visit(MultiplyExpression expr);
-	int visit(DivideExpression expr);
-	int visit(ConstExpression expr);
-}
